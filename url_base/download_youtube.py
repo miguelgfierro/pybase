2032270b19f1c6b1b4696cd3f81aa42aca580827
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-11-27 23:47:18 +0000

    :bug:

--- url_base/download_youtube.py
+++ url_base/download_youtube.py
@@ -9,7 +9,7 @@ def download_youtube(url, work_directory="."):
         url (str): Youtube url.
         work_directory (str): Working directory.
     """
-    os.makedirs(folder, exist_ok=True)
+    os.makedirs(work_directory, exist_ok=True)
     YouTube(url).streams.filter(progressive=True, file_extension="mp4").order_by(
         "resolution"
     ).desc().first().download(work_directory)
