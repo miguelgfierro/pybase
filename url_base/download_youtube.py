421c56a79c998b90b8535647fba2a98441fed819
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-11-29 23:21:32 +0000

    return filepath from download youtube

--- url_base/download_youtube.py
+++ url_base/download_youtube.py
@@ -8,9 +8,18 @@ def download_youtube(url, work_directory="."):
     Args:
         url (str): Youtube url.
         work_directory (str): Working directory.
+
+    Returns:
+        str: Path to the video file.
     """
     os.makedirs(work_directory, exist_ok=True)
-    YouTube(url).streams.filter(progressive=True, file_extension="mp4").order_by(
-        "resolution"
-    ).desc().first().download(work_directory)
+    filepath = (
+        YouTube(url)
+        .streams.filter(progressive=True, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+        .download(work_directory)
+    )
+    return filepath
 
