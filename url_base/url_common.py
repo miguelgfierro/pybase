67cbcc6e8c3b61d1c9a70181729797d266e95ebf
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-12-11 18:44:30 +0000

    cut long lenght image names

--- url_base/url_common.py
+++ url_base/url_common.py
@@ -1,8 +1,9 @@
-def get_image_name(url):
+def get_image_name(url, char_limit=60):
     """Get the file name of an image url.
     
     Args:
         url (str): Image url.
+        char_limit (int): Maximum number of characters for the name.
     
     Returns:
         str: Image name.
@@ -17,13 +18,25 @@ def get_image_name(url):
         >>> url = "https://miguelgfierro.com/static/blog/img/hoaphumanoid"
         >>> get_image_name(url)
         'hoaphumanoid.jpg'
+        >>> url = "https://miguelgfierro.com/012345678901234567890123456789.jpg"
+        >>> get_image_name(url, 20)
+        '01234567890123456789.jpg'
 
     """
-    image_name = str(url[(url.rfind("/")) + 1 :])
-    if "?" in image_name:
-        image_name = image_name[: image_name.find("?")]
+    name = str(url[(url.rfind("/")) + 1 :])
+    if "?" in name:
+        name = name[: name.find("?")]
+
     extensions = (".jpg", ".jpeg", ".gif", ".png", ".bmp", ".svg", ".webp", ".ico")
-    if not any(ext in image_name for ext in extensions):
-        image_name = image_name + ".jpg"
-    return image_name
+    if any(ext in name for ext in extensions):
+        pos = name.rfind(".")
+        ext = name[pos:]
+        name = name[:pos]
+    else:
+        ext = ".jpg"
+
+    if len(name) >= char_limit:
+        name = name[:char_limit]
+
+    return name + ext
 
