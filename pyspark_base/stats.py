2a2de0430dd9fe56b18942452f50454b66384645
Miguel González-Fierro <3491412+miguelgfierro@users.noreply.github.com>
2020-02-06 22:35:32 +0000

    Update stats.py

--- pyspark_base/stats.py
+++ pyspark_base/stats.py
@@ -3,3 +3,4 @@ if __name__ == "__main__":
     print("(rows, columns)={}".format((df.count(), len(df.columns)))) # (rows, columns)=(3,2)
     df.describe().show() # Compute summary statistics
     df.distinct().count() # Count the number of distinct rows
+    df.schema # Return the schema
