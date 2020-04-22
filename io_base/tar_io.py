0929e23173ceea22d32dfd7e5578621d4e01dfb5
Miguel González-Fierro <3491412+miguelgfierro@users.noreply.github.com>
2020-04-22 22:14:14 +0100

    Update tar_io.py

--- io_base/tar_io.py
+++ io_base/tar_io.py
@@ -2,6 +2,6 @@ import os
 import tarfile
 
 def extract_tar_gz(filename, path):
-    with tarfile.open(filename, "r:gz"):
+    with tarfile.open(filename, "r:gz") as tar:
         tar.extractall(path=path)
     return os.path.join(path, filename.split(".tar.gz")[0])
