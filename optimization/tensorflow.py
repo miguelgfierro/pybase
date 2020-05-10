3d2086557025658d357333ce53ac62ff9501dd3b
miguelgfierro <miguelgfierro@users.noreply.github.com>
2020-05-10 20:15:14 +0100

    derivative

--- optimization/tensorflow.py
+++ optimization/tensorflow.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def derivate(func, x):
+def derivate(func, x, val):
     """Perform autodifferentiation.
 
     `See more info <https://adel.ac/automatic-differentiation/>`_
@@ -15,19 +15,18 @@ def derivate(func, x):
         list, float: Derivative and value function.
 
     Examples:
-        >>> x = np.array([1.0, 1.0], dtype=np.float32)
+        >>> x = tf.Variable([0., 0.], tf.float32)
         >>> y = tf.add(tf.pow(tf.subtract(1.0, x[0]), 2.0), tf.multiply(100.0, tf.pow(tf.subtract(x[1],tf.pow(x[0], 2.0)), 2.0)), 'y') # rosenbrock function
-        >>> xd, y = derivate(y, x)
+        >>> val = np.array([1, 1], dtype=np.float32)
+        >>> xd, y = derivate(y, x, val)
         >>> xd
-        [0. 0.]
+        array([0., 0.], dtype=float32)
         >>> y
         0.0
     """
-    var = tf.Variable([0.0, 0.0], tf.float32)
-    dx = tf.gradients(func, var)[0]
+    dx = tf.gradients(func, x)[0]
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        dval = sess.run(dx, {var: x})
-        val = sess.run(func, {var: x})
-        # print(sess.run(func, {var: x}), sess.run(dx, {var: x}))
-    return dval, val
+        dvalue = sess.run(dx, {x: val})
+        value = sess.run(func, {x: val})
+    return dvalue, value
