71c424430dcbad901952e94ef5d181ff2e600727
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-04-11 18:00:10 +0200

    :bug: imread

--- io_base/opencv_io.py
+++ io_base/opencv_io.py
@@ -5,11 +5,11 @@ import urllib.request as urllib
 
 def save_image(img, filename):
     """Save an image with OpenCV.
-    
+
     Args:
         img (numpy array): An image.
         filename (str): Name of the file.
-    
+
     Examples:
         >>> img = cv2.imread('share/Lenna.png')
         >>> save_image(img, 'file.jpg')
@@ -23,28 +23,30 @@ def save_image(img, filename):
     cv2.imwrite(filename, img)
 
 
-def read_image(filename, is_color=True):
+def read_image(filename, flag=cv2.IMREAD_UNCHANGED):
     """Read an image with OpenCV.
-    
+
     Args:
         filename (str): Name of the file.
-        is_color (bool): Read the image in color.
-    
+        flag (int): If -1 (cv2.IMREAD_UNCHANGED), it loads the image as it is including
+            the alpha channel. If 0 (cv2.IMREAD_GRAYSCALE), it loads in grayscale. If
+            1 (cv2.IMREAD_COLOR), it loads a color image, neglecting transparencies.
+
     Returns:
         np.array: An image.
-    
+
     Examples:
         >>> img = read_image('share/Lenna.png')
         >>> shape = np.array(img.shape)
         >>> print(shape)
         [512 512   3]
-        >>> img_gray = read_image('share/Lenna.png', False)
+        >>> img_gray = read_image('share/Lenna.png', 0)
         >>> shape_gray = np.array(img_gray.shape)
         >>> print(shape_gray)
         [512 512]
 
     """
-    return cv2.imread(filename, is_color)
+    return cv2.imread(filename, flag)
 
 
 def read_image_url(url):
