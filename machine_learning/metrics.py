0b9fded7ecff444fd7e2c51545ffafb80afebc01
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-09-29 23:04:07 +0200

    :memo:

--- machine_learning/metrics.py
+++ machine_learning/metrics.py
@@ -357,20 +357,20 @@ def gini(y_true, y_pred):
 
     n_samples = y_true.shape[0]
 
-    # sort rows on prediction column
+    # Sort rows on prediction column
     # (from largest to smallest)
     arr = np.array([y_true, y_pred]).transpose()
     true_order = arr[arr[:, 0].argsort()][::-1, 0]
     pred_order = arr[arr[:, 1].argsort()][::-1, 0]
 
-    # get Lorenz curves
+    # Get Lorenz curves
     l_true = np.cumsum(true_order) / np.sum(true_order)
     l_pred = np.cumsum(pred_order) / np.sum(pred_order)
     l_ones = np.linspace(1 / n_samples, 1, n_samples)
 
-    # get Gini coefficients (area between curves)
+    # Get Gini coefficients (area between curves)
     g_true = np.sum(l_ones - l_true)
     g_pred = np.sum(l_ones - l_pred)
 
-    # normalize to true Gini coefficient
+    # Normalize to true Gini coefficient
     return g_pred / g_true
