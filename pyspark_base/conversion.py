8e60a9071d7c188d37159d1f260e86b906111c98
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-01-17 18:23:27 +0100

    conversion

--- pyspark_base/conversion.py
+++ pyspark_base/conversion.py
@@ -1,5 +1,7 @@
-import pyspark.sql.functions as F
-
+try:
+    from pyspark.sql import functions as F
+except ImportError:
+    pass  # so the environment without spark doesn't break
 
 def replace_column_values(df, to_replace, value, col_name, new_col_name=None):
     """
@@ -11,30 +13,31 @@ def replace_column_values(df, to_replace, value, col_name, new_col_name=None):
         col_name (_type_): _description_
         new_col_name (_type_, optional): _description_. Defaults to None.
 
-    Examples:
-        >>> df = spark.createDataFrame([("a", 1), ("a", 2), ("c", 3)], ["letters", "numbers"])
-        >>> df_return = replace_column_values(df, "a", 1, "letters")
-        >>> df_return.toPandas()
+    **Examples**
+
+        >> df = spark.createDataFrame([("a", 1), ("a", 2), ("c", 3)], ["letters", "numbers"])
+        >> df_return = replace_column_values(df, "a", 1, "letters")
+        >> df_return.toPandas()
           letters  numbers
         0       1        1
         1       1        2
         2       c        3
-        >>> df_return = replace_column_values(df, "a", 1, "letters", "new_column")
-        >>> df_return.toPandas()
+        >> df_return = replace_column_values(df, "a", 1, "letters", "new_column")
+        >> df_return.toPandas()
           letters  numbers new_column
         0       a        1          1
         1       a        2          1
         2       c        3          c
-        >>> df = spark.createDataFrame([(None, 1), ("a", 2), ("c", 3)], ["letters", "numbers"])
-        >>> df_return = replace_column_values(df, None, 1, "letters")
-        >>> df_return.toPandas()
+        >> df = spark.createDataFrame([(None, 1), ("a", 2), ("c", 3)], ["letters", "numbers"])
+        >> df_return = replace_column_values(df, None, 1, "letters")
+        >> df_return.toPandas()
           letters  numbers
         0       1        1
         1       a        2
         2       c        3
-        >>> df = spark.createDataFrame([("a", float("nan")), ("a", 2.0), ("c", 3.0)], ["letters", "numbers"])
-        >>> df_return = replace_column_values(df, float("nan"), 0.0, "numbers")
-        >>> df_return.toPandas()
+        >> df = spark.createDataFrame([("a", float("nan")), ("a", 2.0), ("c", 3.0)], ["letters", "numbers"])
+        >> df_return = replace_column_values(df, float("nan"), 0.0, "numbers")
+        >> df_return.toPandas()
           letters  numbers
         0       a      0.0
         1       a      2.0
