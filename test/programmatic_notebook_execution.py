f59e94de35ba5de978ab06ac76c5f2e84acd93a2
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-10-18 12:53:00 +0200

    Test string

--- test/programmatic_notebook_execution.py
+++ test/programmatic_notebook_execution.py
@@ -1,12 +1,18 @@
 import os
 import sys
+import pytest
+
 
 sys.path.append(os.path.join(".."))  # import pybase
 from pybase.notebooks.notebook_utils import execute_notebook, read_notebook
 
 
-def test_notebook_execution_int():
-    input_notebook = os.path.join("test", "programmatic_notebook_execution.ipynb")
+@pytest.fixture
+def input_notebook():
+    return os.path.join("test", "programmatic_notebook_execution.ipynb")
+
+
+def test_notebook_execution_int(input_notebook):
     execute_notebook(
         input_notebook,
         "output.ipynb",
@@ -16,3 +22,15 @@ def test_notebook_execution_int():
 
     results = read_notebook("output.ipynb")
     assert results["response1"] == 7
+
+
+def test_notebook_execution_letter(input_notebook):
+    execute_notebook(
+        input_notebook,
+        "output.ipynb",
+        kernel_name="python3",
+        parameters=dict(b="M"),
+    )
+
+    results = read_notebook("output.ipynb")
+    assert results["response2"] is True
